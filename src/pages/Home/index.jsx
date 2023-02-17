c3571ef3651315a158404eb195d983fa78958f5e
Déborah <deborah.bude@gmail.com>
2023-02-17 16:48:39 +0100

    Incorporation fichier json

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import '../../index.css';
+import React from "react";
+import "../../index.css";
+import logementsData from "../../logements.json";
 
 export default function Home() {
-  return (
-    <main className="corps-page">
-      <section className="titre__home">
-        <h1>Chez vous, partout et ailleurs</h1>
-      </section>
-      <section>
-          <ul>
-              <li>Logement 1</li>
-              <li>Logement 2</li>
-              <li>Logement 3</li>
-          </ul>
-      </section>
-    </main>
-  );
+	const logements = logementsData.map((logement) => console.log(logement));
+	console.log(logements);
+	return (
+		<main className="corps-page">
+			<section className="titre__page home">
+				<h1>Chez vous, partout et ailleurs</h1>
+			</section>
+			<section>
+				<ul>
+					<li>Logement 1</li>
+					<li>Logement 2</li>
+					<li>Logement 3</li>
+				</ul>
+			</section>
+		</main>
+	);
 }
