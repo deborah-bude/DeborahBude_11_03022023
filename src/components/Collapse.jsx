fb89686abc9a650a2789881a189f7a4b5f66c30a
Déborah <deborah.bude@gmail.com>
2023-02-23 20:21:31 +0100

    Usestate pour ouvrir et fermer les collapse au clic

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import { useState } from "react";
 
 export default function Collapse(content_collape) {
-	return (
+	const [isOpen, setIsOpen] = useState(false);
+
+	return isOpen ? (
 		<div className="collapse">
-			<div className="collapse__title">
+			<div onClick={() => setIsOpen(isOpen ? false : true)} className="collapse__title">
 				<p>{content_collape.titre}</p>
 				<i className="fa-solid fa-chevron-down"></i>
 			</div>
 			<div className="collapse__content">{content_collape.content}</div>
+		</div>
+	) : (
+		<div className="collapse">
+			<div onClick={() => setIsOpen(isOpen ? false : true)} className="collapse__title">
+				<p>{content_collape.titre}</p>
+				<i className="fa-solid fa-chevron-up"></i>
+			</div>
 		</div>
 	);
 }
