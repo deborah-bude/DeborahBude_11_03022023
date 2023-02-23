677078367ed6577c0771516aefff350d6e54d690
Déborah <deborah.bude@gmail.com>
2023-02-23 17:14:38 +0100

    Modification du composant "Collapse" avec niveau des className et des balises

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
 export default function Collapse(content_collape) {
-  return (
-    <li className="collapse">
-        <div className="collapse__title">
-            <p>{content_collape.titre}</p>
-            <i class="fa-solid fa-chevron-down"></i>
-        </div>
-        <div className="collapse__content">
-            <p>{content_collape.content}</p>
-        </div>
-    </li>
-  );
+	return (
+		<div className="collapse">
+			<div className="collapse__title">
+				<p>{content_collape.titre}</p>
+				<i className="fa-solid fa-chevron-down"></i>
+			</div>
+			<div className="collapse__content">{content_collape.content}</div>
+		</div>
+	);
 }
