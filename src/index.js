f364693e253262acb9dde6e4e7f915a6d75361f8
Déborah <deborah.bude@gmail.com>
2023-03-03 18:11:18 +0100

    Finition responsive

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./scss/main.scss";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
